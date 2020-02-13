8948133dfefdee6b7e7a97709709eb12acc9c0ba
Neto Leal <neto.leal@loft.com.br>
2020-02-13 10:04:39 -0300

    feat: termplate finished

--- src/pages/homepage/home.page.js
+++ src/pages/homepage/home.page.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './home.page.scss';
 import { useTranslation } from 'react-i18next';
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 /* PLOP_INJECT_IMPORT */
-import Contact from './contact';
 import HomePage from './homepage';
 
 export {
   /* PLOP_INJECT_EXPORT */
-	Contact,
-	HomePage,
-}
+  HomePage,
+};
